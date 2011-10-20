285e08c8111f407f016a1fa759803c5afadb638a
ToeMann09@gmail.com <ToeMann09@gmail.com@69c156f2-2266-12b7-5f58-8a40f42accc5>
2011-10-20 16:07:42 +0000

    Add command-line args.

--- Main.java
+++ Main.java
@@ -1,5 +1,54 @@
+import java.lang.NumberFormatException;
+
 public class Main {
-    public static void main(String[] parameters) {
-        new Pong();
+    public static void main(String[] argv) {
+        //Each arg takes both a switch and a value
+        if(argv.length % 2 != 0){
+            System.err.println("Usage: java Main <-p paddletype> <-t tickduration> <-s speedup>");
+            System.exit(1);
+        }
+
+        //Set default parameters
+        Pong.PaddleType type = Pong.PaddleType.Human;
+        double tickDuration = 1/10.0;
+        double speedup = 10000;
+
+        //Parse parameters
+        for(int i = 0; i < argv.length; i++){
+            if(argv[i].equals("-p")){//Paddle Type
+                i++;
+                if(argv[i].equals("Genetic"))
+                    type = Pong.PaddleType.Genetic;
+                else if(argv[i].equals("AI"))
+                    type = Pong.PaddleType.AI;
+                else if(argv[i].equals("CPU"))
+                    type = Pong.PaddleType.CPU;
+                else if(argv[i].equals("Human"))
+                    type = Pong.PaddleType.Human;
+                else{
+                    System.err.println("Invalid paddle type");
+                    System.exit(1);
+                }
+            } else if(argv[i].equals("-t")){//TickDuration
+                i++;
+                try{
+                    tickDuration = Double.parseDouble(argv[i]);
+                //Error handling
+                }catch(NumberFormatException e){
+                    System.err.println("Invalid tick duration");
+                    System.exit(1);
+                }
+            } else if(argv[i].equals("-s")){//Speedup
+                i++;
+                try{
+                    speedup = Double.parseDouble(argv[i]);
+                //Error handling
+                }catch(NumberFormatException e){
+                    System.err.println("Invalid speedup");
+                    System.exit(1);
+                }
+            }
+        }
+        new Pong(type, tickDuration, speedup);
     }
 }
